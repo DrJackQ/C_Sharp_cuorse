b379eb69833e5292e9ac278a17a114c8b3e06d63
DrJackQ <DrJackQ@example.com>
2022-07-13 14:18:32 +0300

    домашняя работа задача 2

---
+++ 1_Lesson/1_6/Program.cs
@@ -0,0 +1,6 @@
+﻿int N = int.Parse(Console.ReadLine());
+while (N)
+{
+     Console.WriteLine(n);
+     N%=0;
+} 
---
+++ 1_Lesson/Задача_2/Program.cs
@@ -0,0 +1,10 @@
+﻿int N1 = int.Parse(Console.ReadLine());
+int N2 = int.Parse(Console.ReadLine());
+if (N1 > N2)
+{
+    Console.WriteLine(N1);
+}
+else
+{
+    Console.WriteLine(N2);
+}
