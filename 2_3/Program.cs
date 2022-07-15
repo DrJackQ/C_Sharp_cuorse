39337cd38a29f4d8325c1efef45bd2791c68f532
DrJackQ <DrJackQ@example.com>
2022-07-15 13:43:42 +0300

    Задача 3

---
+++ 2_3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+void OneNum(int num1, int num2)
+{
+    if (num1 % num2 == 0)
+    {
+        Console.WriteLine($"{num1}, {num2} -> Числа кратные");
+    }
+    else
+    {
+        Console.WriteLine($"{num1}, {num2} -> Не кратные, остаток {num1%num2}");
+    }
+}
+
+OneNum(16, 4);
+OneNum(41, 4);
