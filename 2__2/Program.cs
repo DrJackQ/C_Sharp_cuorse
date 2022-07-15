8db09adbb3a1d1ddc4c9dc799208e6d0a8b2c53d
DrJackQ <DrJackQ@example.com>
2022-07-15 11:58:25 +0300

    Задача 2

---
+++ 2__2/Program.cs
@@ -0,0 +1,12 @@
+﻿int Takenum1(int num)
+{
+    if (num % 10 > num / 10)
+    {
+        return num % 10;
+    }
+    else
+        return num / 10;
+}
+int SecondNumber = new Random().Next(10, 100);
+Console.WriteLine (SecondNumber);
+Console.WriteLine (Takenum1(SecondNumber));
