7b0e6f4ec1e44cd7b401512f5304222a30065098
DrJackQ <DrJackQ@example.com>
2022-07-15 14:05:11 +0300

    Задача 4

---
+++ 2_4/Program.cs
@@ -0,0 +1,12 @@
+﻿void GO(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine($"{num} Кратно");
+    }
+    else
+    {
+        Console.WriteLine($"{num} Не кратно");
+    }
+}
+GO(161);
